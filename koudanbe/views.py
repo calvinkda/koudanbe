05b34189bbbd01b2247635e902c83dbb9aa05838
calvinkda <koudanbe@gmail.com>
2020-02-25 16:03:56 +0100

    ajout du html

--- koudanbe/views.py
+++ koudanbe/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
 # Create your views here.
 def home(resquest):
-    text = """
-
-    <h1> KOUDANBE.CM </h1>
+    text= """
+    <h1> teste de la page koudanbe.cm depuis koudanbe merde </h1>
     """
     return HttpResponse(text)
 def acceuil(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
