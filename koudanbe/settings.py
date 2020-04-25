839d32c5295cf594cf07e43d5df44d4e98066dba
calvinkda <koudanbe@gmail.com>
2020-04-25 22:44:41 +0100

    modification du 25 avril

--- koudanbe/settings.py
+++ koudanbe/settings.py
@@ -1,6 +1,6 @@
 
 import os
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,4 +143,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
